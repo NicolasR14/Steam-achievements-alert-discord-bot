6aa3f22d2f176c9bbe17e14b520034f78ac7867d
NicolasR14 <atserio@live.fr>
2024-06-02 16:45:14 -0400

    Fix list game overlenght issue

--- src/commands/setup/list_games.js
+++ src/commands/setup/list_games.js
@@ -1,20 +1,32 @@
 const { SlashCommandBuilder } = require('discord.js');
 
 function list_games(guildId, games) {
-	var header = 'Games I listen to for new achievements :\n'
-	let data = games.filter(g => g.guilds.includes(guildId)).map(g => [g.realName, g.id, [g.name, ...g.aliases].join(',')])
+	let data = games.filter(g => g.guilds.includes(guildId)).map(g => [g.realName, g.id, ((g.name + ',' + g.aliases.join(',')).length > 90) ? ((g.name + ',' + g.aliases.join(',')).slice(0, 85)) : (g.name + ',' + g.aliases.join(','))])
 	data = [['Name', 'Steam ID', 'Reference & Aliases'], ['', '', ''], ...data]
 	let colWidths = data[0].map((_, colIndex) => {
 		return Math.max(...data.map(row => row[colIndex].length));
 	});
+	let messages = [];
+	let message = `\`\`\`\n`
+	let row;
 
-	let formattedLines = data.map(row => {
-		return row.map((item, colIndex) => {
+	for (let i = 0; i < data.length; i++) {
+		const game = data[i]
+		row = game.map((item, colIndex) => {
 			return item.padEnd(colWidths[colIndex], ' ');
 		}).join('\t');
-	}).join('\n');
-
-	return `${header}\`\`\`\n${formattedLines}\n\`\`\``
+		if ((message + row).length < 1980) {
+			message += '\n' + row
+		}
+		else {
+			messages.push(`${message}\n\`\`\``)
+			message = `\`\`\`\n` + row
+		}
+		if (i === data.length - 1) {
+			messages.push(`${message}\n\`\`\``)
+		}
+	}
+	return messages
 }
 
 module.exports = {
@@ -23,6 +35,14 @@ module.exports = {
 		.setDescription('Lists the games the bot listens to for new achievements'),
 
 	async execute(interaction, globalVariables) {
-		await interaction.reply(list_games(interaction.guildId, globalVariables.Games));
+		const messages = list_games(interaction.guildId, globalVariables.Games);
+		for (let i = 0; i < messages.length; i++) {
+			if (i == 0) {
+				await interaction.reply(`Games I listen to for new achievements :${messages[i]}`);
+			}
+			else {
+				interaction.channel.send(messages[i])
+			}
+		}
 	},
 };
